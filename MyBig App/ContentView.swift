1d43184d194cef33d97c1fd8fe25c7e195464eb8
Deyana Chen <dchen@lcs.on.ca>
2026-03-02 11:53:46 -0500

    Initial Commit

---
+++ MyBig App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBig App
+//
+//  Created by chen tianyi on 2026-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBig App/MyBig_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBig_AppApp.swift
+//  MyBig App
+//
+//  Created by chen tianyi on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct MyBig_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
