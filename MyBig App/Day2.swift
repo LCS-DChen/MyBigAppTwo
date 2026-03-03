c9e74c11041964f75491790814b51241548a3820
Deyana Chen <dchen@lcs.on.ca>
2026-03-03 15:17:49 -0500

    commit

---
+++ MyBig App/Day2.swift
@@ -0,0 +1,18 @@
+//
+//  Day2.swift
+//  MyBig App
+//
+//  Created by chen tianyi on 2026-03-03.
+//
+
+import SwiftUI
+
+struct Day2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Day2()
+}
